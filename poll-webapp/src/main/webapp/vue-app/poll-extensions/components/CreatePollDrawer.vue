92d468c1ee578d3eed2eea191d1877e376f87faf
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-02-14 15:05:42 +0100

    Add poll drawer initial content

--- poll-webapp/src/main/webapp/vue-app/poll-extensions/components/CreatePollDrawer.vue
+++ poll-webapp/src/main/webapp/vue-app/poll-extensions/components/CreatePollDrawer.vue
@@ -11,25 +11,71 @@
       </div>
     </template>
     <template slot="content">
-      <div class="attachmentsContent pt-0 pa-5">
-        <form @submit.prevent="onSubmit">
-        </form>
+      <div class="pt-0 pa-5 my-5">
+        <v-form
+          ref="agendaEventForm"
+          class="flex"
+          flat
+          @submit.prevent="onSubmit">
+          <v-list
+            class="d-flex flex-column "
+            dense>
+            <v-list-item
+              class="px-0"
+              dense>
+              <input
+                placeholder="Ask something..."
+                type="text"
+                name="title"
+                class="ignore-vuetify-classes pa-6 px-3 mb-5"
+                style="width : 100%">
+            </v-list-item>
+            
+            <v-list-item
+              v-for="(option, index) in options"
+              :key="index"
+              class="px-0 d-flex"
+              dense>
+              <div class=" float-left mb-4 me-3 " style="width : 25px">
+                <v-btn
+                  v-if="option.removable"
+                  icon
+                  @click="removeOption(option)">
+                  <i class="fas fa-trash-alt" style="color: #5f708a"></i>
+                </v-btn>
+              </div> 
+              <input
+                :placeholder="`Option ${option.id}`"
+                type="text"
+                name="title"
+                class="ignore-vuetify-classes pa-6 px-3 mb-5"
+                style="width : 100%">
+            </v-list-item>
+
+            <v-list-item
+              class="px-0 d-flex justify-end"
+              dense>
+              <div class="d-flex flex-row ">
+                <a class="text-subtitle-1 font-weight-bold" @click="addOption">
+                  + Add option
+                </a>
+              </div>
+            </v-list-item>
+          </v-list>
+        </v-form>
       </div>
     </template>
     <template slot="footer">
-      <div class="d-flex my-2 justify-content-end" style="justify-content : flex-end">
+      <div class="d-flex my-2 flex-row justify-end">
         <v-btn
-          style="border: 1px solid #000;"
-          class="mx-5 px-8"
+          class="mx-5 px-8 btn"
           button
           large
-          color="btn-outlined no-box-shadow"
           @click="cancelCreatePollDrawer">
           Cancel
         </v-btn>
         <v-btn
-          class="px-8"
-          color="primary btn no-box-shadow"
+          class="px-8 primary btn no-box-shadow"
           button
           large
           @click="onSubmit">
@@ -42,6 +88,13 @@
 
 <script>
 export default {
+
+  data(){
+    return {
+      options: [{id: 1,removable: false,data: {}},
+        {id: 2,removable: false,data: {}}]
+    };
+  },
 
   computed: {
     isMobile() {
@@ -59,12 +112,11 @@ export default {
     openCreatePollDrawer(){
       this.$refs.createPollDrawer.open();
       // listner is activated in Social module 'ExoActivityComposer.vue'
-      // to inform that the drawer is opened and then prevent closing the activity composer drawer
+      // to inform that this drawer is opened and then prevent closing the activity composer drawer
       this.$root.$emit('poll-app-drawer-opened');
     },
     cancelCreatePollDrawer(){
       this.$refs.createPollDrawer.close();
-      console.log('closed !');
     },
     onSubmit(){
       this.cancelCreatePollDrawer();
@@ -74,6 +126,15 @@ export default {
       // to prevent closing the activity composer drawer
       this.$root.$emit('poll-app-drawer-closed');
     },
+    addOption(){
+      this.options.push({id: this.options.length+1,removable: true,data: {}});
+    },
+    removeOption(option){
+      this.options.splice(this.options.indexOf(option), 1);
+      this.options.forEach((element,index) => {
+        element.id = ++index;
+      });
+    }
   }
 };
 </script>
