8ebe422c64209dee664bee50a5bbdc4598e0630a
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-02-28 13:56:59 +0100

    Apply new Activity Composer changes

---
+++ poll-webapp/src/main/webapp/vue-app/poll-extensions/components/CreatePollComposer.vue
@@ -0,0 +1,43 @@
+<!--
+This file is part of the Meeds project (https://meeds.io/).
+ 
+Copyright (C) 2022 Meeds Association contact@meeds.io
+ 
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <div>
+    <div class="actionItem" @click="openCreatePollDrawer">
+      <div class="actionItemIcon">
+        <div class="createPollComposerIcon"></div>
+      </div>
+      <div class="actionItemDescription">
+        <div class="actionLabel">{{ $t('composer.poll.create.drawer.label') }}</div>
+        <div class="actionDescription">
+          <p>{{ $t('composer.poll.create.drawer.description') }}</p>
+        </div>
+      </div>
+    </div>
+    <create-poll-drawer ref="createPollDrawer" />
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    openCreatePollDrawer() {
+      this.$refs.createPollDrawer.openDrawer();
+    },
+  },
+};
+</script> 
--- poll-webapp/src/main/webapp/vue-app/poll-extensions/pollExtensions.js
+++ poll-webapp/src/main/webapp/vue-app/poll-extensions/pollExtensions.js
@@ -16,36 +16,12 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-const pollActivityComposerPlugin = {
-  key: 'poll',
-  rank: 40,
-  resourceBundle: 'locale.portlet.Poll',
-  labelKey: 'composer.poll.create.drawer.label',
-  description: 'composer.poll.create.drawer.description',
-  iconClass: 'createPollComposerIcon',
-  enabled: true,
-  component: {
-    name: 'create-poll-drawer',
-    props: {
-    },
-    model: {
-      value: [],
-      default: []
-    },
-    events: []
-  },
-  onExecute: (createPollDrawerComponent) => {
-    createPollDrawerComponent.openDrawer();
-  }
-};
 
 export function initExtensions() {
-  extensionRegistry.registerExtension('ActivityComposer', 'activity-composer-action', pollActivityComposerPlugin);
-
-  document.addEventListener('poll-composer-button-text',(data)=>{
-    pollActivityComposerPlugin.labelKey = data.detail.labelKey;
-    pollActivityComposerPlugin.description = data.detail.description;
-  
+  extensionRegistry.registerComponent('ActivityComposerFooterAction', 'activity-composer-footer-action', {
+    id: 'createPollButton',
+    vueComponent: Vue.options.components['create-poll-composer'],
+    rank: 40,
   });
   
 }
