44848fdea627f99b1ef84225691fe6fedecaeb98
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-09-15 12:55:50 +0100

    feat: Delete used deprecated Spring configuration constants - MEED-7488 - Meeds-io/MIPs#147

--- poll-webapp/src/main/java/io/meeds/poll/PollApplication.java
+++ poll-webapp/src/main/java/io/meeds/poll/PollApplication.java
@@ -27,8 +27,7 @@ import io.meeds.spring.kernel.PortalApplicationContextInitializer;
   AvailableIntegration.KERNEL_MODULE,
   AvailableIntegration.JPA_MODULE,
   AvailableIntegration.LIQUIBASE_MODULE,
-  AvailableIntegration.WEB_SECURITY_MODULE,
-  AvailableIntegration.WEB_TRANSACTION_MODULE,
+  AvailableIntegration.WEB_MODULE,
 })
 @EnableJpaRepositories(basePackages = PollApplication.MODULE_NAME)
 @PropertySource("classpath:application.properties")
